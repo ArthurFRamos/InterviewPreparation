5daa62b3553c47de8a8e9d5c7f5b47ef99eeb322
ArthurFRamos <arthur742@gmail.com>
2017-05-09 11:24:47 -0300

    481 - Magical String

---
+++ LeetCode/481 -  Magical String.java
@@ -0,0 +1,49 @@
+public class Solution {
+    public int magicalString(int n) {
+        StringBuffer buffer = new StringBuffer("122");
+        
+        if(n == 1 || n == 2 || n == 3)
+            return 1;
+        boolean isOne = true;
+        
+        int numberOfOnes = 1;
+        
+        for(int i = 2; buffer.length() < n; i++)
+        {
+            int current = Character.getNumericValue(buffer.charAt(i));
+            
+            String temp = null;
+            
+            if(isOne)
+            {
+                if(current == 1)
+                {
+                    temp = "1";
+                    numberOfOnes++;
+                }
+                else
+                {
+                    temp = "11";
+                    numberOfOnes += 2;   
+                }
+            }
+            else
+            {
+                if(current == 1)
+                    temp = "2";
+                else
+                    temp = "22";
+            }
+            
+            buffer.append(temp);
+            
+            isOne = !isOne;
+        }
+        
+        
+        if(buffer.length() > n && buffer.charAt(n) == '1')
+            numberOfOnes -= 1;
+        
+        return numberOfOnes;
+    }
+}
