279a59d00e180b3ebbb2901dcce93d0e616bdaee
ArthurFRamos <arthur742@gmail.com>
2018-07-18 20:05:27 -0300

    random fun

---
+++ LeetCode/783 - Minimum Distance Between BST Nodes.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    ArrayList<Integer> list = new ArrayList<>();
+    public int minDiffInBST(TreeNode root) {
+        getNodes(root);
+        Collections.sort(list);
+        int min = Integer.MAX_VALUE;
+        
+        for(int i = list.size() - 1; i > 0; i--)
+        {
+            int current = list.get(i) - list.get(i-1);
+            if(current < min)
+                min = current;
+        }
+        
+        return min;
+    }
+    
+    public void getNodes (TreeNode node)
+    {
+        if(node == null)
+            return;
+        
+        list.add(node.val);
+        getNodes(node.left);
+        getNodes(node.right);
+    }
+}
